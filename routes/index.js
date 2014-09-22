06f1ab407fea5b89cf59ceabb5c065afadeb32c9
Your Name <you@example.com>
2014-09-22 10:48:09 -0400

    Updates

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var OAuth = require('oauth');
 var md5 = require('MD5');
 
-function getTwitterClient(req){
+function getTwitterClient(credentials){
   var oauth = new OAuth.OAuth(
       'https://api.twitter.com/oauth/request_token',
       'https://api.twitter.com/oauth/access_token',
-      req.query.consumer_key,
-      req.query.consumer_secret,
+      credentials.consumer_key,
+      credentials.consumer_secret,
       '1.0A',
       null,
       'HMAC-SHA1'
@@ -58,24 +58,31 @@ function update(req, doc, existing, data){
   existing.updated_at = new Date()
   req.db.get('cache').update({lookup: doc.lookup}, existing, {w:1}, function(err, result) {console.log(err);console.log("Updated Document")});
 }
-
-router.get('/:service/*:path\.json', function(req, res) {
+function getCredentials(req, callback){
+}
+router.get('/api/:service/*:path\.json', function(req, res) {
   var db = req.db;
   var collection = db.get('cache');
   var apiParams = keys(req.params.service, req)
   var docMeta = {query: apiParams, service: req.params.service, endpoint: req.params.path+req.params['0']}
-  docMeta.lookup = md5(docMeta)
-  console.log(md5(docMeta))
+  var credentials = {}
+  req.db.get('credentials').find({service: req.params.service}, function(err, doc){
+    if (req.query.oauth_token && req.query.oauth_token_secret && req.query.consumer_key && req.query.consumer_secret){
+      credentials = {oauth_token: req.query.oauth_token, oauth_token_secret: req.query.oauth_token_secret, consumer_key: req.query.consumer_key, consumer_secret: req.query.consumer_secret}
+    } else {
+      credentials = doc[0]
+    }
+  })
+  docMeta.lookup = md5(JSON.stringify(docMeta))
   collection.find({lookup: docMeta.lookup}, function(e,doc){
-    console.log(doc[0] && doc[0].updated_at <= new Date(new Date()-1000*60*60*24*7))
     if (doc[0] && doc[0].updated_at > new Date(new Date()-1000*60*60*24*7)){
       console.log("Responded with Stored.")
       res.send(doc[0].content)
     } else if (doc[0] && doc[0].updated_at <= new Date(new Date()-1000*60*60*24*7)){
-      getTwitterClient(req).get(
+      getTwitterClient(credentials).get(
         'https://api.twitter.com/1.1/'+req.params.path+req.params['0']+'.json'+keysStringed(apiParams),
-        req.query.oauth_token,
-        req.query.oauth_token_secret,
+        credentials.oauth_token,
+        credentials.oauth_token_secret,
         function (e, data, resp){
           if (e) console.error(e);
           update(req, docMeta, doc[0], data)
@@ -83,11 +90,10 @@ router.get('/:service/*:path\.json', function(req, res) {
         }
       )
     } else if (doc[0] == null) {
-      console.log(doc)
-      getTwitterClient(req).get(
+      getTwitterClient(credentials).get(
         'https://api.twitter.com/1.1/'+req.params.path+req.params['0']+'.json'+keysStringed(apiParams),
-        req.query.oauth_token,
-        req.query.oauth_token_secret,
+        credentials.oauth_token,
+        credentials.oauth_token_secret,
         function (e, data, resp){
           if (e) console.error(e);
           store(req, docMeta, data)
@@ -97,13 +103,62 @@ router.get('/:service/*:path\.json', function(req, res) {
     }
   });
 });
-router.post('/:service/*:path\.json', function(req, res) {
-return res.json(getTwitterClient(req).get(
-  'https://api.twitter.com/1.1/'+req.params.path+'.json',
-  req.query.oauth_token,
-  req.query.oauth_token_secret,
-  keys(service, req),
-  result(response)));
+router.post('/api/:service/*:path\.json', function(req, res) {
+  var db = req.db;
+  var collection = db.get('cache');
+  var apiParams = keys(req.params.service, req)
+  var docMeta = {query: apiParams, service: req.params.service, endpoint: req.params.path+req.params['0']}
+  var credentials = getCredentials(req)
+  console.log("Credentials:")
+  console.log(credentials)
+  docMeta.lookup = md5(JSON.stringify(docMeta))
+  collection.find({lookup: docMeta.lookup}, function(e,doc){
+    console.log(doc[0] && doc[0].updated_at <= new Date(new Date()-1000*60*60*24*7))
+    if (doc[0] && doc[0].updated_at > new Date(new Date()-1000*60*60*24*7)){
+      console.log("Responded with Stored.")
+      res.send(doc[0].content)
+    } else if (doc[0] && doc[0].updated_at <= new Date(new Date()-1000*60*60*24*7)){
+      getTwitterClient(credentials).post(
+        'https://api.twitter.com/1.1/'+req.params.path+req.params['0']+'.json',
+        credentials.oauth_token,
+        credentials.oauth_token_secret,
+        keysStringed(apiParams),
+        function (e, data, resp){
+          if (e) console.error(e);
+          update(req, docMeta, doc[0], data)
+          res.send(data)
+        }
+      )
+    } else if (doc[0] == null) {
+      console.log(doc)
+      getTwitterClient(credentials).post(
+        'https://api.twitter.com/1.1/'+req.params.path+req.params['0']+'.json',
+        credentials.oauth_token,
+        credentials.oauth_token_secret,
+        keysStringed(apiParams),
+        function (e, data, resp){
+          if (e) console.error(e);
+          store(req, docMeta, data)
+          res.send(data)
+        }
+      )
+    }
+  });
 });
 
+router.get('/:service/drop_credential_set.json', function(req, res){
+  var doc = req.query
+  doc.service = req.params.service
+  req.db.get('credentials').remove(doc);
+  res.send({removed: doc})
+})
+router.get('/:service/add_credential_set.json', function(req, res){
+  var doc = req.query
+  doc.service = req.params.service
+  doc.rand = Math.random()
+  req.db.get('credentials').insert(doc, {w:1}, function(err, result){
+    if (err) console.error(err);
+    res.send({added: doc})
+  })
+})
 module.exports = router;
